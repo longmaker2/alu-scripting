7d6bf761bdcb70b9cf0b47ff02500084cb008c4c
longmaker2 <l.deng@alustudent.com>
2023-03-06 02:54:51 +0200

    made corrections in task 1

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""" a function that queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit """
+""" a function that queries the Reddit API and prints the titles
+of the first 10 hot posts listed for a given subreddit """
 import requests
 
 
